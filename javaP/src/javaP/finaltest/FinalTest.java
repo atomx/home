a07afefcd407967004c12893604ebfd54b90b969
nexu <nexu@L-SHC-00437104.corp.ebay.com>
2014-03-01 14:23:39 +0800

    Final test

---
+++ javaP/src/javaP/finaltest/FinalTest.java
@@ -0,0 +1,28 @@
+package javaP.finaltest;
+
+public class FinalTest {
+	
+	private final Val value = new Val(3);
+	private static final Val VAL = new Val(5);
+	public static void main(String[] args) {
+		
+		FinalTest t = new FinalTest();
+		//t.value = new Val(1);  can not change reference
+		System.out.println(t.VAL.toString());
+	}
+
+}
+
+class Val{
+	private int i;
+	
+	public Val(int a){
+		i=a;
+	}
+	
+	public String toString(){
+		return "value is: " + i;
+	}
+	
+}
+
