249191994d8b2e291120e18692c3979046e2bdc3
nexu <nexu@L-SHC-00437104.corp.ebay.com>
2014-03-05 21:31:02 +0800

    inner class test

--- javaP/src/javaP/finaltest/FinalTest.java
+++ javaP/src/javaP/finaltest/FinalTest.java
@@ -4,9 +4,12 @@ public class FinalTest {
 	
 	private final Val value = new Val(3);
 	private static final Val VAL = new Val(5);
+	public final String s = "test";
+	
 	public static void main(String[] args) {
 		
 		FinalTest t = new FinalTest();
+		
 		//t.value = new Val(1);  can not change reference
 		System.out.println(t.VAL.toString());
 	}
---
+++ javaP/src/javaP/innerclass/controller/Controller.java
@@ -0,0 +1,25 @@
+package javaP.innerclass.controller;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Controller {
+	private List<Event> eventList = new ArrayList<Event>();
+	public void addEvent(Event e){
+		eventList.add(e);
+	}
+	
+	public void run(){
+		
+		while(eventList.size()>0){
+			for(Event e: new ArrayList<Event>(eventList)){
+				if(e.ready()){
+					System.out.println(e);
+					e.action();
+					eventList.remove(e);
+				}
+			}
+		}
+	}
+	
+}
